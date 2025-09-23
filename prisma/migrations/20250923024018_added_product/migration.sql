3edd3c6b54c89230c809fa8a818f04b9c7e4e127
Thiago Vale <thiagopvale@hotmail.com>
2025-09-22 23:44:36 -0300

    create database tables

---
+++ prisma/migrations/20250923024018_added_product/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DECIMAL(65,30) NOT NULL,
+    "stock" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
